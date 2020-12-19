ec5b6395ced186b09d70104308905545d507b101
Elias F. Fysken <git@fyksen.net>
2020-12-19 19:47:55 +0000

    Add defenitions.v

---
+++ defenitions.v
@@ -0,0 +1,14 @@
+
+`define OPCODE_OP       7'b0110011
+`define OPCODE_OP_IMM   7'b0010011
+`define OPCODE_BRANCH   7'b1100011
+
+`define FUNCT3_ADDSUB   3'b000
+`define FUNCT3_SLL      3'b001
+`define FUNCT3_SLT      3'b010
+`define FUNCT3_SLTU     3'b011
+`define FUNCT3_XOR      3'b100
+`define FUNCT3_SRLA     3'b101
+`define FUNCT3_OR       3'b110
+`define FUNCT3_AND      3'b111
+
